e007814f0d0020677d27142ca1d0cabc4a32fa6c
mariojor <mariojorge.sdei@gmail.com>
2021-08-07 19:14:57 -0300

    Tags

--- Assets/Scripts/AplicarForca.cs
+++ Assets/Scripts/AplicarForca.cs
@@ -16,7 +16,11 @@ public class AplicarForca : MonoBehaviour
     
     void Update()
     {
-        //Objeto que vai receber a forca + o vector + em que direcao a forca vai ser aplicada + o tipo da forca
-        if (Input.GetKeyDown(KeyCode.Space)) { bola.AddForce(new Vector2(0, forca * Time.deltaTime), ForceMode2D.Impulse); }
+        //Informando para game que 
+        if (this.gameObject.CompareTag("mario"))
+        {
+            //Objeto que vai receber a forca + o vector + em que direcao a forca vai ser aplicada + o tipo da forca
+            if (Input.GetKeyDown(KeyCode.Space)) { bola.AddForce(new Vector2(0, forca * Time.deltaTime), ForceMode2D.Impulse); }
+        }
     }
 }
