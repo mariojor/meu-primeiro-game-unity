be2ed653bea89d0f4f984c5f373c8d6efffc94bf
mariojor <mariojorge.sdei@gmail.com>
2021-08-07 18:26:14 -0300

    Aprendendo sobre RigidBody e removendo arquivos desnecessarios da branch

---
+++ Assets/Scripts/AplicarForca.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AplicarForca : MonoBehaviour
+{
+
+    public float forca = 500f;
+    public Rigidbody2D bola;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //Objeto que vai receber a forca + o vector + em que direcao a forca vai ser aplicada + o tipo da forca
+        if (Input.GetKeyDown(KeyCode.Space)) { bola.AddForce(new Vector2(0, forca * Time.deltaTime), ForceMode2D.Impulse); }
+    }
+}
